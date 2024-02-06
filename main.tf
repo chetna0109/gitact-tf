091c63b49963f5dc8443537b2641f05ae0cdd712
Joshi <chandrika.joshi@accenture.com>
2024-02-06 12:05:11 +0530

    asxdi

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws"
     region="ap-south-1"
 }
 module "ec2_instance"{
-    source=".modules/ec2_instance"
+    source="gitact-tf/modules/ec2_instance"
     ami_value="ami-03f4878755434977f"
     instance_type="t2.micro"
 }
